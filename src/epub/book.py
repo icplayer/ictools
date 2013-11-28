f946cf01801bafe06ee5d8bf665ee6a9c5c5bd9e
Krzysztof Langner <klangner@gmail.com>
2013-11-28 15:22:44 +0100

    Added folders for content

--- src/epub/book.py
+++ src/epub/book.py
@@ -18,21 +18,30 @@ def createBookFolders(rootFolder, lesson):
     os.mkdir(rootFolder)
     createMimeTypeFile(rootFolder, 'application/epub+zip')
     createMetaInfFolder(rootFolder)
-    createContentFolder(rootFolder)
-    createPackageDocument(rootFolder, lesson)
+    contentFolder = createContentFolder(rootFolder)
+    createPackageDocument(contentFolder, lesson)
+    copyRuntime(contentFolder)
+    createCSSFiles(contentFolder, lesson)
+    createMediaFiles(contentFolder, lesson)
+    createHtmlFiles(contentFolder, lesson)
 
 
 def createMimeTypeFile(rootFolder, content):
-    _writeTemplate(rootFolder, 'mimetype', {})
+    _writeTemplate(rootFolder + "/mimetype", 'mimetype', {})
 
 
 def createMetaInfFolder(rootFolder):
     metaFolder = rootFolder + '/META-INF'
     os.mkdir(metaFolder)
-    _writeTemplate(metaFolder, 'container.xml', {})
+    _writeTemplate(metaFolder + "/container.xml", 'container.xml', {})
 
 
-def _writeTemplate(destFolder, templateName, params):
+def copyRuntime(contentFolder):
+    runtimeFolder = contentFolder + '/runtime'
+    os.mkdir(runtimeFolder)
+
+
+def _writeTemplate(destFilename, templateName, params):
     ''' Parse template parameters and write contents to given folder 
         with the same name like template
     '''
@@ -41,7 +50,7 @@ def _writeTemplate(destFolder, templateName, params):
     templateFile.close()
     for key,value in params.iteritems():
         content = content.replace('{{' + key + '}}', value)
-    outFile = open(destFolder + "/" + templateName, 'w')
+    outFile = open(destFilename, 'w')
     outFile.write(content)
     outFile.close()
 
@@ -49,16 +58,52 @@ def _writeTemplate(destFolder, templateName, params):
 def createContentFolder(rootFolder):
     contentFolder = rootFolder + '/content'
     os.mkdir(contentFolder)
+    return contentFolder
     
     
-def createPackageDocument(rootFolder, lesson):
-    contentFolder = rootFolder + '/content'
-    _writeTemplate(contentFolder, 'book.opf', {'title':lesson.name, 
-                                               'items':'', 
-                                               'itemrefs': '',
-                                               'modified_date': ''})    
+def createPackageDocument(contentFolder, lesson):
+    items = _createManifestItems(lesson)
+    spine = _createSpineItems(lesson)
+    _writeTemplate(contentFolder + "/book.opf", 
+                   'book.opf', 
+                   {'title':lesson.name, 'items':items, 'itemrefs': spine, 'modified_date': ''})    
 
 
+def _createManifestItems(lesson):
+    items = ''
+    for i in range(len(lesson.pages)):
+        pageId = "page%d" % (i+1)
+        items += "<item id='%s' href='xhtml/%s.html' media-type='application/xhtml+xml'/>\n" % (pageId, pageId)
+    return items
+
+
+def _createSpineItems(lesson):
+    items = ''
+    for i in range(len(lesson.pages)):
+        pageId = "page%d" % (i+1)
+        items += "<itemref idref='%s'/>\n" % pageId
+    return items
+
+
+def createCSSFiles(contentFolder, lesson):
+    cssFolder = contentFolder + '/css'
+    os.mkdir(cssFolder)
+
+
+def createMediaFiles(contentFolder, lesson):
+    mediaFolder = contentFolder + '/media'
+    os.mkdir(mediaFolder)
+
+
+def createHtmlFiles(contentFolder, lesson):
+    htmlFolder = contentFolder + '/xhtml'
+    os.mkdir(htmlFolder)
+    for i in range(len(lesson.pages)):
+        pageId = "page%d" % (i+1)
+        destFile = "%s/%s.html" % (htmlFolder, pageId)
+        _writeTemplate(destFile, 'page.html', {})
+
+    
 def cleanBookFolders(rootFolder):
     if os.path.exists(rootFolder):
         shutil.rmtree(rootFolder)
